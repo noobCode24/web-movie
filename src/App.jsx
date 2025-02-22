d4cacdbc30190076106c5515749d5a064eefcbbc
noobCode24 <vusun2k2004@gmail.com>
2025-02-23 03:18:25 +0700

    Init project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <main className='text-lg text-red-500 font-bold'>
+        react
+      </main>
+    </>
+  )
+}
+
+export default App
